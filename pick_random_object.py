791d43cb1c7154d9f3ac484594345fff03da0293
EthanMetz <95163017+EthanMetz@users.noreply.github.com>
2022-04-23 18:39:41 +0000

    This is the random.choice() function in action. The main branch is a longer way to do the same operation.

--- pick_random_object.py
+++ pick_random_object.py
@@ -6,12 +6,5 @@ names = names_string.split(", ")
 
 #Write your code below this line 👇
 
-#Gets the number of names in the list
-number_of_names = len(names)
-
-#finds a random number, searching 0 through (denoted by the -1) the number of names
-random_number = random.randint(0, number_of_names -1)
-#searches the list for the [object that matches the random_number
-payer = names[random_number] 
-print(payer + "Is paying today.")
-
+buyer = random.choice(names)
+print(buyer + " Is paying today.")
